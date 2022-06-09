9b16f5f9740afe6d39556bc7c9f4ec5a5a5463cc
Tanner Robinson <Rob16041@byui.edu>
2022-06-09 13:50:55 -0600

    Add tables to context

--- Data/RazorPagesDeskQuoteContext.cs
+++ Data/RazorPagesDeskQuoteContext.cs
@@ -13,4 +13,7 @@ using MegaDeskRazor.Models;
         }
 
         public DbSet<MegaDeskRazor.Models.DeskQuote>? DeskQuote { get; set; }
+        public DbSet<MegaDeskRazor.Models.RushOrder>? RushOrder { get; set; }
+        public DbSet<MegaDeskRazor.Models.DesktopMaterial>? DesktopMaterial { get; set; }
+        public DbSet<MegaDeskRazor.Models.Desk>? Desk { get; set; }
     }
